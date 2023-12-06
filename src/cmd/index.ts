e397ff03b4332b460a97c1027cd9335168914cce
4lch4 <git@4lch4.email>
2023-12-06 03:51:25 -0600

    chore: executed prettier

--- src/cmd/index.ts
+++ src/cmd/index.ts
@@ -1,5 +1,5 @@
-import { TogglCommand } from './Toggl'
 import { SmokesCommand } from './Smokes'
+import { TogglCommand } from './Toggl'
 
 export async function buildCommands() {
   const togglCommand = await new TogglCommand().build()
